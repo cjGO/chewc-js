eedd2def226abecb56cb9246a389496b073998bf
cjgo <cltngwn@gmail.com>
2024-12-14 17:30:10 -0600

    remove components

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import FounderPool from '$lib/components/FounderPool.svelte';
-	import FounderIndividualPool from '$lib/components/FounderIndividualPool.svelte';
-</script>
-
-<div class="container mx-auto space-y-8">
-	<FounderPool />
-	<FounderIndividualPool />
-</div>
