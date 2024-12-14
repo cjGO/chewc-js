0056746a2d20ac832a2554f64234191e393f9406
cjgo <cltngwn@gmail.com>
2024-12-14 15:22:03 -0600

    move poisson

--- src/lib/sim/core.ts
+++ src/lib/sim/core.ts
@@ -42,17 +42,3 @@ export function createFounderIndividual(
     paternalChromosome
   };
 }
-
-
-export function samplePoisson(lambda: number): number {
-  let L = Math.exp(-lambda);
-  let k = 0;
-  let p = 1;
-  
-  do {
-    k++;
-    p *= Math.random();
-  } while (p > L);
-  
-  return k - 1;
-}
